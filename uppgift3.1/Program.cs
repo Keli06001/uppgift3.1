520cede514bde5da93488cd3311edbfd56924d07
Keli06001 <keli06001@utb.vaxjo.se>
2023-10-04 10:59:11 +0200

    Add project files.

---
+++ uppgift3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Uppgift3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int gammal=int.Parse(Console.ReadLine());
+
+            if (gammal<16)
+            {
+                Console.WriteLine("Du får inte vara med eftersom du är för ung");
+            }
+            else if (gammal > 19)
+            {
+                Console.WriteLine("Du får inte vara med eftersom du är för gammal");
+            }
+            else if (gammal>=16 &&  gammal <= 19)
+            {
+                Console.WriteLine("Du får vara med på tävlingen");
+            }
+            Console.ReadKey();
+        }
+    }
+}
